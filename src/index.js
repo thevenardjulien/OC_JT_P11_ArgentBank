8ddb4c1589ee35c8e3499e4d75347f73ab9cba45
THEVENARD Julien <thevenard.julien@gmail.com>
2024-01-12 08:20:25 +0100

    Set-up React Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/sign-in",
+    element: "Sign-in page",
+  },
+  {
+    path: "/user",
+    element: "User page",
+  },
+  {
+    path: "*",
+    element: "Impossible de trouver la page",
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
