ce95e41b0b9efef81621a8df1096537f98d28879
THEVENARD Julien <thevenard.julien@gmail.com>
2024-01-16 22:08:51 +0100

    Account Component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./style.scss";
 
-const Account = () => {
-  return <div></div>;
+const Account = ({ accounts }) => {
+  return (
+    <div>
+      {accounts &&
+        accounts.map((account, index) => {
+          return (
+            <section className="account">
+              <div className="account-content-wrapper">
+                <h3 className="account-title">{account.title}</h3>
+                <p className="account-amount">{account.amount}</p>
+                <p className="account-amount-description">
+                  {account.description}
+                </p>
+              </div>
+              <div className="account-content-wrapper cta">
+                <button className="transaction-button">
+                  View transactions
+                </button>
+              </div>
+            </section>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Account;
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,9 +2,28 @@ import React from "react";
 import Header from "../../layout/Header";
 import Footer from "../../layout/Footer";
 import Button from "../../components/Button";
+import Account from "../../components/Account";
 import "./style.scss";
 
 const User = () => {
+  const userAccounts = [
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.790",
+      description: "Available Balance",
+    },
+    {
+      title: "Argent Bank Savings (x6712)",
+      amount: "$10,928.42",
+      description: "Available Balance",
+    },
+    {
+      title: "Argent Bank Credit Card (x8349)",
+      amount: "$184.30",
+      description: "Current Balance",
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -15,6 +34,8 @@ const User = () => {
           Tony Jarvis!
         </h1>
         <Button text="Edit Name" link="/user" />
+        <h2 class="sr-only">Accounts</h2>
+        <Account accounts={userAccounts} />
       </main>
       <Footer />
     </>
