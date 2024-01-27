a552bcbfbb5f4cc91228540eeaac007fbdef8a01
THEVENARD Julien <thevenard.julien@gmail.com>
2024-01-27 23:41:38 +0100

    V1 Final

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,13 @@
-import Header from "../../layout/Header";
-import Footer from "../../layout/Footer";
-import Account from "../../components/Account";
-import { userAccounts } from "../../services/datas/userAccounts";
 import { useSelector } from "react-redux";
+import Account from "../../components/Account";
 import ProfileHeader from "../../components/ProfileHeader";
+import Footer from "../../layout/Footer";
+import Header from "../../layout/Header";
+import { userAccounts } from "../../services/datas/userAccounts";
 import "./style.scss";
 
 const Profile = () => {
   const token = useSelector((state) => state.user.value.token);
-
   return (
     <>
       <Header />
